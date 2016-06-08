092db8e159bce27e3639614fd41fe82fd1391c24
Haozhou Wang <hawang@pivotal.io>
2016-06-08 12:11:06 +0800

    Let ListBucketWithRetry be able to retry
    ListBucket() should not throw exception if the error can be resolved by retrying,
    otherwise ListBucketWithRetry() could not retry. Besides, we also add try/catch
    within ListBucketWithRetry() to gurrantee the exception-process coverage and print
    out error message in following codes.
    
    Co-authored by Haozhou Wang and Kuien Liu

--- gpAux/extensions/gps3ext/include/s3interface.h
+++ gpAux/extensions/gps3ext/include/s3interface.h
@@ -26,7 +26,7 @@ class S3Service : public S3Interface {
    private:
     string getUrl(const string& prefix, const string& schema, const string& host,
                   const string& bucket, const string& marker);
-    void parseBucketXML(ListBucketResult* result, xmlNode* root_element, string& marker);
+    bool parseBucketXML(ListBucketResult* result, xmlNode* root_element, string& marker);
     xmlParserCtxtPtr getBucketXML(const string& region, const string& url, const string& prefix,
                                   const S3Credential& cred, const string& marker);
     bool checkAndParseBucketXML(ListBucketResult* result, xmlParserCtxtPtr xmlcontext,
